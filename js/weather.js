ed71ef55b779a3bdc6679f987c57e8fdc7b06805
sim wook <astralkaiser89@gmail.com>
2022-11-04 22:21:39 +0900

    practice javascript

--- js/weather.js
+++ js/weather.js
@@ -1,19 +1,20 @@
 const API_KEY = "ec6f206231e3a8acc036f7e06dcbc0c5";
 
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json().then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     }));
 }
 
 function onGeoError() {
-
+    city.innerText = "場所の情報取得失敗";
+    weather.innerText = "天気の情報取得失敗";
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
